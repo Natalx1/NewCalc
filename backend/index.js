b70c95b5a594d9870f076ca69f55447e3b86959c
Natalx1 <nati6689@gmail.com>
2023-04-12 15:01:58 +0100

    xxx

--- backend/index.js
+++ backend/index.js
@@ -1,38 +1,18 @@
 //https://stackoverflow.com/questions/5823722/how-to-serve-an-image-using-nodejs
 
-var path = require('path');
 var express = require('express');
 var app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
-var options = {
-    index: "myWebPage.html"
-  };
-
-var dir = path.join(__dirname, '../frontend');
-
-app.get('/api', function(req, res){
-    res.send("Yes we have an API now")
-});
-
-// e.g. test using:
-//http://127.0.0.1:8000/api/getPrice?salary=2000&days=20
-app.get('/api/getPrice', function(req, res){
-    //res.send("Hello world!")
-    // Copied from front end
-    var s = req.query.salary;
-    var d = req.query.days;
-    console.log("Calculating price")
-    console.log(s)
-    console.log(d)
-    let finalPrice = 0;
-    dailyRate = s/365;
-    price = Math.round(dailyRate * d);
-    var roundToNearest = 50;
-    roundedPrice = Math.round((price+roundToNearest)/roundToNearest) * roundToNearest // Always round up
-    res.send(""+roundedPrice)
-});
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static(dir, options));
+
+app.get("/", function(req, res){
+    res.send("express is working")
+}
+)
 
 // 404 page
 app.use(function ( req, res, next) {
